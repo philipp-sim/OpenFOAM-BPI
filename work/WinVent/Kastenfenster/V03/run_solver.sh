d56c167b47b1e3215167eae5de3d8fc2dbee065d
philipp-sim <p.e.simlinger@gmail.com>
2025-10-10 12:54:13 +0200

    Status 10/10/2025 12:55

---
+++ work/WinVent/Kastenfenster/V03/run_solver.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+source /opt/openfoam12/etc/bashrc
+
+nprocs=4
+foamDictionary system/decomposeParDict -entry numberOfSubdomains -set $nprocs
+
+rm -rf 0 > /dev/null 2>&1
+cp -r 0.orig 0 > /dev/null 2>&1
+
+./include/createInitialValue.sh
+setFields
+decomposePar -force
+mpirun -np $nprocs foamRun -parallel
+
